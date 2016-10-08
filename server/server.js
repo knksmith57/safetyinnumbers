7faa34e5937e08e61a663c34d715780b8e3c4a6e
Loren Heubert-Aubry <lorenha@umich.edu>
2016-10-08 17:39:23 -0400

    removed duplicate server.js file

--- server/server.js
+++ server/server.js
@@ -1,25 +1,83 @@
+const mqtt = require('mqtt')
+const client = mqtt.connect('mqtt://broker.hivemq.com')
 var firebase = require("firebase");
 
-firebase.initializeApp({
-  serviceAccount: "path/to/serviceAccountCredentials.json",
-  databaseURL: "https://mhacks8-c0c8a.firebaseio.com/"
-});
+var config = {
+    apiKey: "AIzaSyDPPkHh0huH-BIuG2Bik9doADu9jzfGM0g",
+    authDomain: "mhacks8-c0c8a.firebaseapp.com",
+    databaseURL: "https://mhacks8-c0c8a.firebaseio.com",
+    storageBucket: "mhacks8-c0c8a.appspot.com",
+    messagingSenderId: "768473290344",
+    serviceAccount: "MHacks8-5286fa3c6dde.json"
+};
+firebase.initializeApp(config);
 
-// The app only has access to public data as defined in the Security Rules
-var db = firebase.database();
-var ref = db.ref("/some_public_resource");
-ref.once("value", function(snapshot) {
-  console.log(snapshot.val());
-});
+
+
+var state = "detecting";
 
 //data will be stored at this database reference:
+
 var db = firebase.database();
-var ref = db.ref("server/saving-data/fireblog");
+var camsRef = db.ref("cameras");
 
 
-var camsRef = ref.child("cameras");
-camsRef.child().set({
-    cam_number: cam_num,
-    time_activated: trigger_time,
-    location: geodata
+//MQTT
+client.on('connect', () => {
+  //if camera detects people, update density
+  client.subscribe('camera/detecting', function(){
+    console.log("camera is detecting!");
+    var message = {
+      camID: '2',
+      time_activated: new Date(),
+      density: 2
+    };
+
+    client.publish("camera/detecting", JSON.stringify(message));
+    // if camID not recognized, add to firebase
+  });
+  //if camera falls asleep, set density to 0
+  client.subscribe('camera/sleep', function(){
+    console.log("camera is asleep!");
+    var message = {
+      camID: '1',
+      density: 0,
+    };
+    client.publish("camera/sleep", JSON.stringify(message));
+  });
+});
+
+client.on("message", function(topic, message){
+  //message is a buffer
+  console.log("topic: ", topic);
+  console.log("message: ", JSON.parse(message.toString()));
+  message = JSON.parse(message.toString());
+  var camID = message.camID;
+  var timeTriggered = message.time_activated;
+  var density = message.density;
+  if(topic == "camera/detecting"){
+    camsRef.on("value", function(snapshot){
+      if(!(snapshot.hasChild(camID))){
+        camsRef.child(camID).set({
+            cam_number: camID,
+            time_activated: timeTriggered,
+            location: 0, //geodata
+            density: density
+        });
+      }
+      console.log("updating child:");
+      camsRef.child(camID).update({
+        time_activated: timeTriggered,
+        density: density
+      });
+    });
+  }
+  else if(topic == "camera/sleep"){
+    camsRef.on("value", function(snapshot){
+      camsRef.child(camID).update({
+        density: 0
+      });
+    });
+  }
+
 });
